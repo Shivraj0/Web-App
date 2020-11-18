307a8a01e3966c15975a6dc990fa0bb87a937c7d
Shivraj <kolishivraj05@gmail.com>
2020-11-19 02:21:15 +0530

    moved /userlist route method

--- routes/index.js
+++ routes/index.js
@@ -11,18 +11,6 @@ router.get('/home', function(req, res, next) {
   res.render('home', { title: 'Home Page' });
 });
 
-/* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-  var db = req.db;
-  var collection = db.get('usercollection');
-  collection.find({}, {}, function(e, docs) {
-    res.render('userlist', {
-      title: "Userlist",
-      "userlist": docs
-    });
-  });
-});
-
 /* GET New user page. */
 router.get('/newuser', function(req, res) {
   res.render('newuser', {
@@ -35,7 +23,7 @@ router.post('/adduser', function(req, res) {
   // Set internal db variable.
   var db = req.db;
 
-  // Get form values.
+  // Get form values. Rely on name attributes.
   var userName = req.body.username;
   var userEmail = req.body.useremail;
 
